1956ac2ff74c0b24301cb868b0d610390a814f6c
David Störmer <badf00d@posteo.de>
2019-09-08 13:00:05 +0200

    Added basic LogView

---
+++ src/DesktopExtension/DesktopExtension/Log/ILogViewModel.cs
@@ -0,0 +1,47 @@
+﻿using DesktopExtension.Utils;
+using Reactive.Bindings;
+using System;
+
+namespace DesktopExtension.Log
+{
+    public interface ILogViewModel
+    {
+        ReactiveCollection<INotification> Notifications { get;  }
+        ReactiveProperty<INotification> SelectedNotification { get; }
+    }
+
+    class LogViewModel : ILogViewModel, IDisposable
+    {
+        public ReactiveCollection<INotification> Notifications { get; }
+        public ReactiveProperty<INotification> SelectedNotification { get; }
+
+        public LogViewModel()
+        {
+            Notifications = new ReactiveCollection<INotification>();
+            SelectedNotification = new ReactiveProperty<INotification>();
+        }
+
+        public void Dispose()
+        {
+            Notifications?.Dispose();
+            SelectedNotification?.Dispose();
+        }
+    }
+
+    class LogDesignViewModel : LogViewModel
+    {
+        public LogDesignViewModel()
+        {
+            Notifications.AddOnScheduler(new DummyNotification("Some info", NotificationType.Information));
+            Notifications.AddOnScheduler(new DummyNotification("Some warning", NotificationType.Warning));
+            Notifications.AddOnScheduler(new DummyNotification("Some error", NotificationType.Error));
+        }
+
+        private class DummyNotification : ANotification
+        {
+            public DummyNotification(string message, NotificationType type) : base(message, type)
+            {
+            }
+        }
+    }
+}
---
+++ src/DesktopExtension/DesktopExtension/Log/NotificationDateTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DesktopExtension.Utils;
+
+namespace DesktopExtension.Log
+{
+    public class NotificationDateTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NotificationTemplate { get; set; }
+
+
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is INotification notification)
+            {
+
+                return NotificationTemplate;
+            }
+
+            return null;
+        }
+    }
+}
